2416fdb6035d8f86e6c479482f4d9acc7891eb81
ehmusman <ehmusman@gmail.com>
2021-01-03 12:13:16 +0500

    footer component is created

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer bg-dark text-light text-center d-flex flex-column min-vh-70">
+            <p>
+                All rights &copy; are reserved
+            </p>
+        </div>
+    )
+}
+
+export default Footer
