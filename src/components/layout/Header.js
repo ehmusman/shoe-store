da0eff0182dc289288359270bb7810115e769080
ehmusman <ehmusman@gmail.com>
2021-01-04 10:47:08 +0500

    h

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,10 +5,7 @@ function Header() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
             <div className="container">
                 <Link to='/' className="navbar-brand">Shoe Store</Link>
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#navBarNav">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div id="navBarNav" className="collapse navbar-collapse">
+                
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">
