22b9994a71d21fcff4bfc555f2a093fb31e86e69
ehmusman <ehmusman@gmail.com>
2021-01-14 22:50:13 +0500

    shoe store becomes responsive

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,32 +3,34 @@ import { Link } from 'react-router-dom'
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-            <div className="container nav-outer ">
-                <div>
-                    <Link to='/' className="navbar-brand d-block">Shoe Store</Link>
-                </div>
-                <ul className="navbar-nav ml-auto nav-inner">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/">
-                            <i className="fas fa-home"></i>
+            <div className="container">
+                <Link to='/' className="navbar-brand d-block">Shoe Store</Link>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navBarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="navBarNav" class="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">
+                                <i className="fas fa-home"></i>
                                 Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/products">Products</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/contact">Contact</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/cart">
-                            <i className="fas fa-shopping-cart"></i>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/products">Products</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                <i className="fas fa-shopping-cart"></i>
                                 Cart</Link>
-                    </li>
-                </ul>
-
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
 
